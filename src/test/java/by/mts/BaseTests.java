05fbcfda1aa3a0b75623abe3212edf8a31511e7c
eldar <eldar.zaripoff@gmail.com>
2024-07-06 09:50:09 +0300

    corrected L17

--- src/test/java/by/mts/BaseTests.java
+++ src/test/java/by/mts/BaseTests.java
@@ -9,15 +9,17 @@ import java.util.concurrent.TimeUnit;
 
 public class BaseTests {
     WebDriver chromeDriver;
+
     @BeforeEach
     public void setChromeDriver() {
         System.setProperty("webdriver.chrome.driver", System.getenv("CHROME_DRIVER"));
         //System.setProperty("webdriver.chrome.driver", "C:\\Temp\\chromedriver-win64\\chromedriver.exe");
         chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
-        chromeDriver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        chromeDriver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
 
     }
+
     @AfterEach
     public void after() {
         chromeDriver.quit();
--- src/test/java/by/mts/Tests.java
+++ src/test/java/by/mts/Tests.java
@@ -1,78 +1,208 @@
 package by.mts;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.netology.helpers.ExpectedData;
+import ru.netology.helpers.XPaths;
+import ru.netology.pages.MTSMainPage;
 
-import java.time.Duration;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.stream.Stream;
 
 public class Tests extends BaseTests {
     @Test
+    @DisplayName("Проверка названия заголовка")
     public void checkTheTitleOfBlock() {
-        chromeDriver.get("http://mts.by");
-        clickCookieButton();
-        WebElement title = chromeDriver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение')]"));
-        String expected = "Онлайн пополнение\n" +
-                "без комиссии";
-        Assertions.assertEquals(title.getText(), expected, "Заголовок не соответствует требуемому");
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        Assertions.assertEquals(
+                ExpectedData.EXPECTED_TITLE_OF_MTS_MAIN_PAGE,
+                mtsMainPage.getTitle().getText(),
+                "Название заголовка не соответствует требуемому");
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт"})
+    @DisplayName("Проверка наличия логотипов платёжных систем")
+    @ValueSource(strings = {ExpectedData.VISA, ExpectedData.VERIFIED_BY_VISA, ExpectedData.MASTER_CARD,
+            ExpectedData.MASTER_CARD_SECURE_CODE, ExpectedData.BEL_CARD})
     public void checkTheLogoOfBlock(String input) {
-        chromeDriver.get("http://mts.by");
-        clickCookieButton();
-        List<WebElement> logoList = chromeDriver.findElements(By.xpath("//img[contains(@src, " +
-                "'/local/templates/new_design/assets/html/images/pages/index/pay')]"));
-        List<String> logoNames = logoList.stream()
-                .map(webElement -> webElement.getAttribute("alt"))
-                .collect(Collectors.toList());
-        Assertions.assertTrue(logoNames.contains(input), "На странице отсутствуют требуемое изображение: " + input);
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        Assertions.assertTrue(
+                mtsMainPage.getLogoListAsListOfStrings().contains(input),
+                "На странице отсутствуют требуемое изображение: " + input);
+        Assertions.assertTrue(mtsMainPage.getLogoList().stream().allMatch(WebElement::isDisplayed));
     }
 
     @Test
+    @DisplayName("Проверка работы ссылки «Подробнее о сервисе»")
     public void checkTheLink() {
-        chromeDriver.get("http://mts.by");
-        clickCookieButton();
-        String startPage = chromeDriver.getCurrentUrl();
-        WebElement button = chromeDriver.findElement(By.xpath("//a[contains(text(), 'Подробнее о сервисе')]"));
-        button.click();
-        String newPage = chromeDriver.getCurrentUrl();
-        Assertions.assertNotEquals(startPage, newPage, "Ссылка 'Подробнее о сервисе' нерабочая");
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        mtsMainPage.getButtonInDetails().click();
+        Assertions.assertNotEquals(ExpectedData.MTS_LINK,
+                mtsMainPage.getCurrentUrl(),
+                "Ссылка 'Подробнее о сервисе' нерабочая");
     }
 
-    @Test
-    public void checkInputData() {
-        chromeDriver.get("http://mts.by");
-        clickCookieButton();
-        WebElement phoneNumber =
-                chromeDriver.findElement(By.xpath("//div[@class='input-wrapper input-wrapper_label-left']" +
-                        "/input[@class='phone'][../label[@for='connection-phone']]"));
-        phoneNumber.sendKeys("297777777");
-        WebElement sum =
-                chromeDriver.findElement(By.xpath("//div[@class='input-wrapper input-wrapper_label-right']" +
-                        "/input[@class='total_rub'][../label[@for='connection-sum']]"));
-        sum.sendKeys("100");
-        WebElement continueButton =
-                chromeDriver.findElement(By.xpath("//form[@class='pay-form opened']/button[contains(text(), " +
-                        "'Продолжить')]"));
-        continueButton.click();
-        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//iframe[@class='bepaid-iframe']")));
-        Assertions.assertTrue(chromeDriver.findElement(By.xpath("//iframe[@class='bepaid-iframe']")).isDisplayed(),
+    @ParameterizedTest
+    @DisplayName("Проверка работы кнопки «Продолжить» (проверяем только вариант «Услуги связи», номер для теста 297777777)")
+    @MethodSource("provideMapData")
+    public void checkInputData(Map<String, String> inputData) {
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        mtsMainPage.fillCommunicationServices(inputData);
+        mtsMainPage.waitForVisibilityOfFrame();
+        Assertions.assertTrue(mtsMainPage.getIframe().isDisplayed(),
                 "Кнопка 'Продолжить' не работает");
-
     }
 
-    private void clickCookieButton() {
-        WebElement cookieButton = chromeDriver.findElement(By.xpath("//button[@class='btn btn_black cookie__ok']"));
-        cookieButton.click();
+    @ParameterizedTest
+    @DisplayName("""
+            Проверка надписи в незаполненных полях каждого варианта оплаты услуг:\s
+            услуги связи,\s
+            домашний интернет,\s
+            рассрочка,\s
+            задолженность;""")
+    @MethodSource("providePlaceHolders")
+    public void checkPlaceHolders(String option, String phoneXpath, String phoneNumberExpected, String sumXpath,
+                                  String sumExpected, String emailXpath, String emailExpected) {
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        mtsMainPage.getListUnrollButton().click();
+        mtsMainPage.getButtonForSelectOptionInList(option).click();
+
+        /*
+        Проверка плэйсхолдера в поле номера телефона
+         */
+        Assertions.assertTrue(mtsMainPage.getPlaceHolderAsString(phoneXpath).contains(phoneNumberExpected));
+         /*
+        Проверка плэйсхолдера в поле введённой суммы
+         */
+        Assertions.assertTrue(mtsMainPage.getPlaceHolderAsString(sumXpath).contains(sumExpected));
+         /*
+        Проверка плэйсхолдера в поле электронной почты
+         */
+        Assertions.assertTrue(mtsMainPage.getPlaceHolderAsString(emailXpath).contains(emailExpected));
+    }
+
+    @ParameterizedTest
+    @DisplayName("проверить корректность отображения суммы (в том числе на кнопке)," +
+            " номера телефона, " +
+            "а также надписей в незаполненных полях для ввода реквизитов карты, наличие иконок платёжных систем.")
+    @MethodSource("provideMapData")
+    public void checkPopUp(Map<String, String> inputData) {
+        chromeDriver.get(ExpectedData.MTS_LINK);
+        MTSMainPage mtsMainPage = new MTSMainPage(chromeDriver);
+        mtsMainPage.getCookieButton().click();
+        mtsMainPage.fillCommunicationServices(inputData);
+        mtsMainPage.waitForVisibilityOfFrameThroughAttribute();
+        mtsMainPage.switchTheFrame();
+        mtsMainPage.waitForSpanInFrame();
+
+        /*
+        Проверка корректности отображения суммы в pop-up
+         */
+        Assertions.assertEquals(inputData.get("sum"), mtsMainPage.stringModifier(
+                        XPaths.SPAN_CONTAINS_TEXT_BYN_XPATH, "\\.00 BYN", ""),
+                "В pop-up сумма отображается некорректно");
+
+        /*
+        Проверка корректности отображения суммы на кнопке в pop-up
+         */
+        Assertions.assertEquals(inputData.get("sum"), mtsMainPage.stringModifier(
+                        XPaths.BUTTON_CONTAINS_TEXT_BYN_XPATH, "Оплатить |.00 BYN", ""),
+                "На кнопке в pop-up сумма отображается некорректно");
+
+        /*
+        Проверка корректности отображения номера телефона в pop-up
+         */
+        Assertions.assertEquals(inputData.get("phone"),
+                mtsMainPage.stringModifier(XPaths.PHONE_NUMBER_XPATH, ".*?(297777777).*", "$1"),
+                "Placeholder " + inputData.get("phone") + " отсутствует или содержит неверные данные");
+
+        /*
+        Проверка корректности отображения плэйсхоледра в поле 'Номер карты' в pop-up
+         */
+        Assertions.assertEquals(ExpectedData.EXPECTED_CARD_NUMBER,
+                mtsMainPage.getPlaceHolder(XPaths.CARD_NUMBER_XPATH).getText(),
+                "Placeholder 'Номер карты' неверный либо отсутствует");
+
+        /*
+        Проверка корректности отображения плэйсхоледра в поле 'Срок действия' в pop-up
+         */
+        Assertions.assertEquals(ExpectedData.EXPECTED_VALIDITY_PERIOD,
+                mtsMainPage.getPlaceHolder(XPaths.VALIDITY_PERIOD_XPATH).getText(),
+                "Placeholder 'Срок действия' неверный либо отсутствует");
+
+        /*
+        Проверка корректности отображения плэйсхоледра в поле 'CVC' в pop-up
+         */
+        Assertions.assertEquals(ExpectedData.CVC, mtsMainPage.getPlaceHolder(XPaths.CVC_XPATH).getText(),
+                "Placeholder 'CVC' неверный либо отсутствует");
+
+        /*
+        Проверка корректности отображения плэйсхоледра в поле 'Имя держателя' в pop-up
+         */
+        Assertions.assertEquals(ExpectedData.NAME_OF_CARD_HOLDER, mtsMainPage.getPlaceHolder(XPaths.NAME_OF_CARD_XPATH).getText(),
+                "Placeholder 'Имя держателя (как на карте)' неверный либо отсутствует");
+
+        /*
+        Проверка наличия иконок платёжных систем в pop-up
+         */
+        Assertions.assertTrue(mtsMainPage.getIconSourcesAsStrings().stream()
+                .allMatch(source -> ExpectedData.EXPECTED_ICONS.stream()
+                        .anyMatch(source::contains)));
+    }
+
+    /*
+    Наборы аргументов для проверки каждого варианта оплаты услуг:
+    1. Вариант услуги
+    2. Фрагмент xPath для поля номера телефона
+    3. Ожидаемое содержимое Placeholder для поля номера телефона
+    4. Фрагмент xPath для поля вводимой суммы
+    5. Ожидаемое содержимое Placeholder для поля суммы
+    6. Фрагмент xPath для поля электронной почты
+    7. Ожидаемое содержимое Placeholder для поля электронной почты
+     */
+    private static Stream<Arguments> providePlaceHolders() {
+        return Stream.of(
+                Arguments.arguments("Услуги связи",
+                        "connection-phone", "Номер телефона",
+                        "connection-sum", "Сумма",
+                        "connection-email", "E-mail для отправки чека"),
+                Arguments.arguments("Домашний интернет",
+                        "internet-phone", "Номер абонента",
+                        "internet-sum", "Сумма",
+                        "internet-email", "E-mail для отправки чека"),
+                Arguments.arguments("Рассрочка",
+                        "score-instalment", "Номер счета на 44",
+                        "instalment-sum", "Сумма",
+                        "instalment-email", "E-mail для отправки чека"),
+                Arguments.arguments("Задолженность",
+                        "score-arrears", "Номер счета на 2073",
+                        "arrears-sum", "Сумма",
+                        "arrears-email", "E-mail для отправки чека")
+        );
+    }
+
+    /*
+    Вводимые данные в блоке "Услуги связи"
+    Метод создан с целью повторного использования данных в различных тестах
+     */
+    private static Stream<Map<String, String>> provideMapData() {
+        Map<String, String> inputData = Map.of("phone", "297777777", "sum", "100");
+        return Stream.of(inputData);
     }
 }
